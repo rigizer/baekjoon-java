776f3865bb7201f94cb310b7ac5262df2c9ce7f8
rigizer <68414303+rigizer@users.noreply.github.com>
2022-04-06 22:58:33 +0900

    BOJ10171 문제풀이

---
+++ src/BOJ10171.java
@@ -0,0 +1,10 @@
+// https://www.acmicpc.net/problem/10171
+
+public class BOJ10171 {
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
