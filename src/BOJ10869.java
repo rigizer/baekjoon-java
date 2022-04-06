80658f278db362322332305ed12d2f13895d122a
rigizer <68414303+rigizer@users.noreply.github.com>
2022-04-06 23:06:13 +0900

    BOJ10869 문제풀이

---
+++ src/BOJ10869.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/10869
+
+public class BOJ10869 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
+		
+		StringTokenizer st = new StringTokenizer(s);
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		System.out.println(a + b);
+		System.out.println(a - b);
+		System.out.println(a * b);
+		System.out.println(a / b);
+		System.out.println(a % b);
+	}
+}
