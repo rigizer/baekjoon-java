c9d6de2bede8227ab6d3239dbf488bb363c73f53
rigizer <68414303+rigizer@users.noreply.github.com>
2022-04-06 22:53:56 +0900

    BOJ2588 풀이개선

--- src/BOJ2588.java
+++ src/BOJ2588.java
@@ -4,46 +4,16 @@ import java.io.InputStreamReader;
 // https://www.acmicpc.net/problem/2588
 
 public class BOJ2588 {
-	private static int get3(String num1, char cNum2) {
-		int n1 = Integer.parseInt(num1);
-		int n2 = Character.getNumericValue(cNum2);
-		
-		return n1 * n2;
-	}
-	
-	private static int get4(String num1, char cNum2) {
-		int n1 = Integer.parseInt(num1);
-		int n2 = Character.getNumericValue(cNum2);
-		
-		return n1 * n2;
-	}
-	
-	private static int get5(String num1, char cNum2) {
-		int n1 = Integer.parseInt(num1);
-		int n2 = Character.getNumericValue(cNum2);
-		
-		return n1 * n2;
-	}
-	
-	private static int get6(String num1, String num2) {
-		int n1 = Integer.parseInt(num1);
-		int n2 = Integer.parseInt(num2);
-		
-		return n1 * n2;
-	}
-	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		String num1 = br.readLine();
-		String num2 = br.readLine();
+		int num1 = Integer.parseInt(br.readLine());
+		int num2 = Integer.parseInt(br.readLine());
 		br.close();
 		
-		char[] cNum2 = num2.toCharArray();
-		
-		System.out.println(get3(num1, cNum2[2]));
-		System.out.println(get4(num1, cNum2[1]));
-		System.out.println(get5(num1, cNum2[0]));
-		System.out.println(get6(num1, num2));
+		System.out.println(num1 * (num2 % 10));
+		System.out.println(num1 * (num2 % 100 / 10));
+		System.out.println(num1 * (num2 / 100));
+		System.out.println(num1 * num2);
 	}
 }
