65e7a3c49a4074fe4f8c27745a26b731abd931a0
rigizer <68414303+rigizer@users.noreply.github.com>
2022-04-16 20:05:54 +0900

    BOJ2635 풀이개선 (브루트포스 방식으로 변경, 반례 찾아야함)

--- src/BOJ2635.java
+++ src/BOJ2635.java
@@ -2,39 +2,45 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 // https://www.acmicpc.net/problem/2635
 
 public class BOJ2635 {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		Random rd = new Random();
-		
-		//int n = Integer.parseInt(br.readLine());
-		int n = 100;
-		int m = rd.nextInt(n);
-		int idx = 2;
-		
+		int n = Integer.parseInt(br.readLine());
+		int max = 2;
 		List<Integer> list = new ArrayList<>();
-		list.add(n);
-		list.add(m);
 		
-		while(true) {
-			int diff = n - m;
+		for (int m = n / 2; m < n; m++) {
+			int tempN = n;
+			int tempM = m;
 			
-			if (n - m > 0) {
-				list.add(n - m);
-				n = m;
-				m = diff;
-				idx++;
-				continue;
+			List<Integer> tmpList = new ArrayList<>();
+			tmpList.add(tempN);
+			tmpList.add(tempM);
+			
+			while(true) {
+				int diff = tempN - tempM;
+				
+				if (tempN - tempM >= 0) {
+					tmpList.add(tempN - tempM);
+					tempN = tempM;
+					tempM = diff;
+					continue;
+				}
+				
+				break;
 			}
 			
-			break;
+			int size = tmpList.size();
+			if (size > max) {
+				list = tmpList;
+				max = size;
+			}
 		}
 		
-		System.out.println(idx);
+		System.out.println(max);
 		list.stream().forEach(num -> System.out.print(num + " "));;
 		
 		br.close();
