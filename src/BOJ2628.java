ac4f479e52cce22318b350b7de3c39a16a948fd6
rigizer <68414303+rigizer@users.noreply.github.com>
2022-04-17 19:33:59 +0900

    BOJ2628 최대값 구하는 로직 수정

--- src/BOJ2628.java
+++ src/BOJ2628.java
@@ -3,6 +3,7 @@ import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
@@ -10,6 +11,18 @@ import java.util.StringTokenizer;
 // https://www.acmicpc.net/problem/2628
 
 public class BOJ2628 {
+	private static int calc(List<Integer> list) {
+		int tmp = 0;
+		int max = 0;
+		for (Integer i: list) {
+			int diff = Math.abs(tmp - i);
+			max = Math.max(diff, max);
+			tmp = i;
+		}
+		
+		return max;
+	}
+	
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -24,39 +37,36 @@ public class BOJ2628 {
 		
 		// 어디를 자를지 입력받는다
 		int[][] arr = new int[n][2];
-		StringTokenizer st2 = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
+			StringTokenizer st2 = new StringTokenizer(br.readLine());
 			arr[i][0] = Integer.parseInt(st2.nextToken());
 			arr[i][1] = Integer.parseInt(st2.nextToken());
 		}
 		
-		// 가로 및 세로 최대값을 저장할 공간을 할당한다
-		int garoMax = 0;
-		int seroMax = 0;
-		
-		// 각각의 가로 넓이들을 구한다
-		List<Integer> widthMax = new ArrayList<>();
-		int widthTemp = 0;
-		for (int i = 0; i < w; i++) {
-			if (i == arr[i][0]) {
-				widthMax.add(widthTemp - i);
-				widthTemp = arr[i][0];
+		// 가로로 잘라야 하는 좌표를 리스트화 한다 (세로좌표)
+		List<Integer> garoList = new ArrayList<>();
+		garoList.add(0);
+		garoList.add(h);
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i][0] == 0) {
+				garoList.add(arr[i][1]);
 			}
 		}
+		Collections.sort(garoList);
 		
-		// 각각의 세로 넓이들을 구한다
-		List<Integer> heightMax = new ArrayList<>();
-		int heightTemp = 0;
-		for (int i = 0; i < h; i++) {
-			if (i == arr[i][0]) {
-				heightMax.add(heightTemp - i);
-				heightTemp = arr[i][1];
+		// 세로로 잘라야 하는 좌표를 리스트화 한다 (가로좌표)
+		List<Integer> seroList = new ArrayList<>();
+		seroList.add(0);
+		seroList.add(w);
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i][0] == 1) {
+				seroList.add(arr[i][1]);
 			}
 		}
+		Collections.sort(seroList);
 		
-		// 가로, 세로의 최대값을 구한다
-		garoMax = Collections.max(widthMax);
-		seroMax = Collections.max(heightMax);
+		int garoMax = calc(garoList);
+		int seroMax = calc(seroList);
 		
 		// 가로 * 세로, 즉 넓이를 출력한다
 		bw.write(String.valueOf(garoMax * seroMax));
