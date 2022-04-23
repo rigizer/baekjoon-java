c95b219c66c916e9c97503b511e5da0eaa4a0521
rigizer <68414303+rigizer@users.noreply.github.com>
2022-04-24 00:22:55 +0900

    BOJ2741 문제풀이

---
+++ src/BOJ2741.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/2741
+
+public class BOJ2741 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		
+		for (int i = 1; i <= n; i++) {
+			System.out.println(i);
+		}
+	}
+}
