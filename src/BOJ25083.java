dc8ac4756aa20301186d7ea55f8117ce7f7cbeee
rigizer <68414303+rigizer@users.noreply.github.com>
2022-05-09 21:00:19 +0900

    BOJ25083 문제풀이

---
+++ src/BOJ25083.java
@@ -0,0 +1,12 @@
+// https://www.acmicpc.net/problem/25083
+
+public class BOJ25083 {
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
