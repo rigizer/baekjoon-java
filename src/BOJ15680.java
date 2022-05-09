f17322207c91e8753d473e808ed4d1f2138ba6bd
rigizer <68414303+rigizer@users.noreply.github.com>
2022-05-09 21:06:32 +0900

    BOJ15680 문제풀이

---
+++ src/BOJ15680.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/15680
+
+public class BOJ15680 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		switch(n) {
+		case 0:
+			System.out.println("YONSEI");
+			break;
+		case 1:
+			System.out.println("Leading the Way to the Future");
+			break;
+		}
+	}
+}
