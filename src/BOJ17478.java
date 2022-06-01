f9c12bd5a4d3a7cfaabe7572df33e13b8fc5ee9c
rigizer <68414303+rigizer@users.noreply.github.com>
2022-06-01 21:42:30 +0900

    BOJ17478 풀이개선

--- src/BOJ17478.java
+++ src/BOJ17478.java
@@ -3,42 +3,27 @@ import java.util.Scanner;
 // https://www.acmicpc.net/problem/17478
 
 public class BOJ17478 {
-	private static void printUnderbar(int n) {
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < 4; j++) {
-				System.out.print("_");
-			}
-		}
-	}
+	private static String underbar = "";
 	
-	private static void recursive(int n, int max) {
-		printUnderbar(n);
-		System.out.println("\"재귀함수가 뭔가요?\"");
+	private static void recursive(int n) {
+		String line = underbar;
 		
-		if (n == max) {
-			printUnderbar(n);
-			System.out.println("\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-			
-			printUnderbar(n);
-			System.out.println("라고 답변하였지.");
+		System.out.println(line + "\"재귀함수가 뭔가요?\"");
 		
+		if (n == 0) {
+			System.out.println(line + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
+			System.out.println(line + "라고 답변하였지.");
 			return;
 		}
 		
+		System.out.println(line + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+		System.out.println(line + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
+		System.out.println(line + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
 		
-		printUnderbar(n);
-		System.out.println("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
+		underbar += "____";
+		recursive(n - 1);
 		
-		printUnderbar(n);
-		System.out.println("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-		
-		printUnderbar(n);
-		System.out.println("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
-		
-		recursive(++n, max);
-		
-		printUnderbar(--n);
-		System.out.println("라고 답변하였지.");
+		System.out.println(line + "라고 답변하였지.");
 	}
 	
 	public static void main(String args[]) throws Exception {
@@ -46,7 +31,7 @@ public class BOJ17478 {
 		int n = sc.nextInt();
 		
 		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
-		recursive(0, n);
+		recursive(n);
 		
 		sc.close();
 	}
