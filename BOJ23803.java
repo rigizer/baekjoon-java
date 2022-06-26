1ff6014ff4a576323976e6475aa023616f4f4880
rigizer <68414303+rigizer@users.noreply.github.com>
2022-06-26 22:38:18 +0900

    BOJ23803 문제풀이

---
+++ BOJ23803.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+// https://www.acmicpc.net/problem/23803
+
+public class BOJ23803 {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+
+		for (int i = 0; i < 5; i++) {
+			if (i == 4) {
+				for (int j = 0; j < n; j++) {
+					for (int k = 0; k < n; k++) {
+						for (int l = 0; l < 5; l++) {
+							System.out.print("@");
+						}
+					}
+
+					System.out.println();
+				}
+			} else {
+				for (int j = 0; j < n; j++) {
+					for (int k = 0; k < n; k++) {
+						System.out.print("@");
+					}
+
+					System.out.println();
+				}
+			}
+		}
+
+		sc.close();
+	}
+}
