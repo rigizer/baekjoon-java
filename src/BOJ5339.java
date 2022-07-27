e8231f1d19cf492a9968e0ffe8ff104db40eebb8
rigizer <68414303+rigizer@users.noreply.github.com>
2022-07-27 09:17:10 +0900

    BOJ5339 문제풀이

---
+++ src/BOJ5339.java
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/5339
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		System.out.println("     /~\\");
+		System.out.println("    ( oo|");
+		System.out.println("    _\\=/_");
+		System.out.println("   /  _  \\");
+		System.out.println("  //|/.\\|\\\\");
+		System.out.println(" ||  \\ /  ||");
+		System.out.println("============");
+		System.out.println("|          |");
+		System.out.println("|          |");
+		System.out.println("|          |");
+	}
+}
